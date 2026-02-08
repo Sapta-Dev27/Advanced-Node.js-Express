c073d56f6b3225c672f29ec62e50b8c432ffbdcf
Sapta-Dev27 <149397021+Sapta-Dev27@users.noreply.github.com>
2026-02-08 12:54:02 +0530

    first commit

---
+++ AdvancedExpress/server.js
@@ -0,0 +1,97 @@
+import express from 'express';
+const PORT = 8001
+const app = express(); // invoke the app 
+
+//health check route
+app.get('/health', (request, response) => {
+  response.send('SERVER IS LIVE')
+})
+
+//users array
+const users = [
+  {
+    id: 1,
+    email: 'sapta1@gmail.com'
+  },
+  {
+    id: 2,
+    email: 'sapta2@gmail.com'
+  },
+  {
+    id: 3,
+    email: 'sapta3@gmail.com'
+  },
+]
+
+//products array
+const products = [
+  {
+    id: 1,
+    name: 'Iphone 15'
+  },
+  {
+    id: 2,
+    name: 'Iphone 16'
+  },
+  {
+    id: 3,
+    name: 'Iphone 17'
+  },
+]
+
+//get : /users/email  => route
+app.get('/users/email', (request, response) => {
+  return response.status(200).json({
+    users_list_email: users.map((x) => x.email)
+  })
+})
+
+// get : /users => route
+app.get('/users', (request, response) => {
+  return response.status(200).json({
+    users_list: users
+  })
+})
+
+// get : /products => route 
+app.get('/products', (request, response) => {
+  return response.status(200).json({
+    products_list: products
+  })
+})
+
+// get : /products/:id => route
+app.get('/products/:id', (request, response) => {
+  const parsedId = parseInt(request.params.id);
+  if (isNaN(parsedId)) {
+    return response.status(400).json({
+      success: false,
+      message: 'Bad Request . Pls send some proper ID !!'
+    })
+  }
+  const fetchedProduct = products.find((x) => x.id === parsedId);
+  if (!fetchedProduct) {
+    return response.status(404).json({
+      success: false,
+      message: 'Product is not found in the LIST !!'
+    })
+  }
+  return response.status(200).json({
+    success: true,
+    message: 'Fetched Product successfully',
+    product_fetched_from_db: fetchedProduct
+  })
+})
+
+
+/*'GET' request : 1st parameter : /health => route ;  2nd Parameter : () => {} // callback function  ; (request , response) :
+request object => gets the Headers from client side from req.headers , gets the IP addreess , gets the data from client side from req.body
+and response is a object that takes the data from the server side ( maybe database) and returns it to client side ( maybe JSON , TEXT , HTML)
+*/
+
+
+app.listen(PORT, () => {
+  console.log(`SERVER IS RUNNING ON ${PORT}`)  // listen the app , return a callback function to start the server
+})
+
+
---
+++ AdvancedNode.js/Basics.md
@@ -0,0 +1,13 @@
+# Key Features of Nodejs 
+1. Open Source Tool 
+2. Cross Platform Tool ( works on all devices : MAC , WINDOWS)
+3. Uses chrome's v8 engiene to run JS code outside the browser 
+Hence , Nodejs is basically neither a FRAMEWORK , nor a LIBRARY  , nor a LANGUAGE . It is basically a JS runtime environment made using 
+chrome's v8 engiene to run JS code outside browser . It make JS code to run on the server side .
+4.Nodejs is basically not suitable for those task where it requires high CPU intensive .
+5. Nodejs follows the event driven architechture : single threaded , non  block IO model 
+
+# Difference of Nodejs and JS 
+1. Nodejs => runs on server , database connectivity possible and file system access is possible 
+2. Javascript => runs on browser , database connectivity is not possible and file system access is also restricted 
+
